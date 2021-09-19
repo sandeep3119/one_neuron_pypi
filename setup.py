7c102147fb0a9f55d3a5b91ddcb565baf22b5cd4
Sandeep Kumar Rana <rana.sandeep3119@gmail.com>
2021-09-19 18:01:50 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+PROJECT_NAME ="oneNeuron_pypi"
+USERNAME="sandeep3119"
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USERNAME}",
+    version="0.0.1",
+    author=USERNAME,
+    author_email="rana.sandeep3119@example.com",
+    description="Its a implementation of a Perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USERNAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USERNAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
